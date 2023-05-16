899bfb99a3384e01c638085c7129b4e93ef5bec9
hammarhank <axtr@live.com>
2023-05-16 15:49:55 +0200

    grov skiss på funktioner.

--- CurrencyExplore.py
+++ CurrencyExplore.py
@@ -1,1 +1,17 @@
+import tkinter as tk
+from tkinter import ttk
+import requests
+import json
 
+class CurrencyConverter(tk.Tk):
+    
+    def __init__(self):
+        #TODO: rita upp guit med tkinter
+        0
+
+    def get_currencies(self):
+        #TODO: hämta valutavärden från exchangerate.host
+        0
+    def convert(self):
+        #Konvertera mellan olika valutor.
+        0
